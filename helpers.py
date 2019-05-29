7e723e2ad3f32e6b92c8e110a17da876b5a1942f
Zev Landau <zev@landausoftware.com>
2019-05-29 11:42:11 -0400

    Implemented function to match equal lines in 2 strings

--- helpers.py
+++ helpers.py
@@ -1,8 +1,12 @@
 def lines(a, b):
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    # Use `set` to filter out duplicates
+    lines_a = set(a.split('\n'))
+    lines_b = set(b.split('\n'))
+
+    matches = [line for line in lines_a if line in lines_b]
+    return matches
 
 
 def sentences(a, b):
